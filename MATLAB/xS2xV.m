d45f14c7696fa307f4a4e6d227f2e2201c9a0d7b
tmlouw <tmlouw@sun.ac.za>
2022-03-02 09:49:33 +0200

    Updated files structure

--- MATLAB/xS2xV.m
+++
@@ -1,6 +0,0 @@
-function xV = xS2xV(xS, fields)
-n = length(fields);
-xV = zeros(n, 1);
-for i = 1:n
-    xV(i) = xS.(fields{i}); 
-end
--- MATLAB/xV2xS.m
+++
@@ -1,5 +0,0 @@
-function xS = xV2xS(xV, fields)
-n = length(fields);
-for i = 1:n
-    xS.(fields{i}) = xV(i,:);
-end
