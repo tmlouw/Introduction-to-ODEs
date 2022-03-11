85866517c2b3841c38d897874dce6f003860904d
tmlouw <tmlouw@sun.ac.za>
2022-03-12 00:16:22 +0200

    Implemented most of "Introdcution to ODEs" in Python

--- Python/Simulation_functions.py
+++ Python/Simulation_functions.py
@@ -1,14 +1,56 @@
+import types
 import numpy as np
 
+def reactor_intermediate_variables(t, x, u, p):
+    # Unpack vector
+    V = x[0]
+    nA = x[1]
+    nB = x[2]
+    nC = x[3]
 
-def ode_motion(t, x):
-    v = x[0]
-    s = x[1]
-    k = 1
-    m = 1000
-    F = -k * s
-    dvdt = F / m
-    dsdt = v
+    # Calculate intermediate values
+    v = types.SimpleNamespace()
+    v.cA3 = nA / V
+    v.cB3 = nB / V
+    v.cC3 = nC / V
 
-    dxdt = np.array([dvdt, dsdt])
-    return dxdt
+    v.h = V / p.A
+    v.q3 = p.cV*np.sqrt(np.abs(v.h))
+    v.nA1 = u.q1(t)*u.cA1(t)
+    v.nB2 = u.q2(t)*u.cB2(t)
+    v.nA3 = v.q3   *v.cA3
+    v.nB3 = v.q3   *v.cB3
+    v.nC3 = v.q3   *v.cC3
+
+    if type(t) == float:
+        r = np.zeros([2, 1])
+    else:
+        r = np.zeros([2, t.size])
+
+    r[0,:] = p.k1 *v.cA3
+    r[1,:] = p.k2f*v.cA3*v.cB3**2 - p.k2r*v.cC3
+
+    v.S = p.Nu.dot(r)
+
+    return v
+
+def reactor_ode(t, x, u, p):
+    # Unpack vector
+    V = x[0]
+    nA = x[1]
+    nB = x[2]
+    nC = x[3]
+
+    v = reactor_intermediate_variables(t, x, u, p)
+
+    dV_dt  = u.q1(t) +  u.q2(t) - v.q3  + v.S[0]
+    dnA_dt = v.nA1              - v.nA3 + v.S[1]
+    dnB_dt =            v.nB2   - v.nB3 + v.S[2]
+    dnC_dt =                    - v.nC3 + v.S[3]
+
+    dx_dt = np.empty_like(x)
+    dx_dt[0] = dV_dt
+    dx_dt[1] = dnA_dt
+    dx_dt[2] = dnB_dt
+    dx_dt[3] = dnC_dt
+    return dx_dt
--- Python/main.py
+++ Python/main.py
@@ -1,15 +1,62 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.integrate import solve_ivp
+#%% Clear and import
+from IPython import get_ipython
+get_ipython().run_line_magic('reset', '-f')
+
 from Simulation_functions import *
+import types
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
 
-t = np.arange(0, 1200)
-sol = solve_ivp(ode_motion, [0, t[-1]], [1, 0], t_eval = t)
+#%% Define time range and parameters
+t = np.arange(0, 1200) # s, time over which to peform integration
 
+p = types.SimpleNamespace()
+p.cV  = 0.045  # m2.5/s, Outlet flowrate coefficient
+p.A   = 2      # m2, Cross-sectional area of reactor
+p.k1  = 5.0e-2 # 1/s, Reaction 1 rate constant
+p.k2f = 2.5e+0 # m6/mol2.s, Reaction 2 forward rate constant
+p.k2r = 5.0e-2 # 1/s, Reaction 2 reverse rate constant
 
-plt.style.use('seaborn-poster')
-plt.subplot(211)
-plt.plot(sol.t, sol.y[0])
-plt.subplot(212)
-plt.plot(sol.t, sol.y[1])
+# Matrix of stoichiometric coefficients for each state variable
+p.Nu = np.array([[0, 0], [-1, -1], [1, -2], [0, 1]])
+
+#%% Define exogeneous variables
+u = types.SimpleNamespace()
+u.q1  = lambda t: 0.02
+u.q2  = lambda t: 0.01 + 0.05 * (t > 400)
+u.cA1 = lambda t: 1.50 + 0.50 * (t > 800)
+
+y = np.zeros(t.size)
+for i in range(1, t.size):
+    y[i]  = 0.8*y[i-1] + 0.05*np.random.normal()
+
+u.cB2 = interp1d(t, y + 2)
+
+#%% Integrate ODE
+x0 = np.empty(4)
+x0[0] = 0.9     # m3, initial tank volume
+x0[1] = 0.15    # mol/m3, initial concentration of A
+x0[2] = 0.25    # mol/m3, initial concentration of A
+x0[3] = 0.30    # mol/m3, initial concentration of A
+
+x0 = np.array([0.9, 0.15, 0.25, 0.30])
+sol = solve_ivp(lambda t, x: reactor_ode(t, x, u, p),
+                [0, t[-1]], x0, t_eval = t)
+
+V = sol.y[0]
+nA = sol.y[1]
+nB = sol.y[2]
+nC = sol.y[3]
+v = reactor_intermediate_variables(t, sol.y, u, p)
+
+#%% Plot the solution to the problem
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.plot(sol.t, v.h)
+
+ax2 = fig.add_subplot(212)
+ax2.plot(sol.t, v.cA3)
+ax2.plot(sol.t, v.cB3)
+ax2.plot(sol.t, v.cC3)
 plt.show()
